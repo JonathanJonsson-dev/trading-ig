5d7fbcb583ea2fdf5d8f3f337c3206baea02aff8
Jonathan Jonsson <JonathanJonsson-dev@users.noreply.github.com>
2025-11-16 15:31:52 +0100

    Enhance rest sample to fetch epics alongside Norgate tickers

--- sample/rest_ig.py
+++ sample/rest_ig.py
@@ -6,6 +6,7 @@ IG Markets REST API sample with Python
 2015 FemtoTrader
 """
 
+import norgatedata
 from trading_ig import IGService
 from trading_ig.config import config
 import logging
@@ -26,28 +27,31 @@ def fetch_epics(ig_service):
     """
     Min gamla kod som hämtar epics innan jag insåg att det går att gör via filen all_nodes.py
     """
-    search_terms = ["EUR/USD", "Ether", "USA Tech100", "Tyskland 40", "Naturgas", "US Treasury Bond", "3-Month SOFR"]
-    #search_terms = pd.read_excel('ig_markets_marknader.xlsx', sheet_name="Blad2")['Namn']
-    
-    search_terms = pd.read_excel('ig_markets_marknader.xlsx', sheet_name="Norgate tickers")['Namn']
-    #search_terms = ["Australian Dollar", "British Pound", "Canadian Dollar", "Euro FX", "E-mini S&P 500", "Nasdaq 100"]
+    #search_terms = ["EUR/USD", "Ether", "USA Tech100", "Tyskland 40", "Naturgas", "US Treasury Bond", "3-Month SOFR"]
+    search_terms = pd.read_excel('manuell_berakning_minimum_kapital.xlsx', sheet_name="Norgate tickers")['Namn']
+    norgate_tickers = pd.read_excel('manuell_berakning_minimum_kapital.xlsx', sheet_name="Norgate tickers")['Code']
     epics = []
     markets_that_got_no_result = []
     
-    for term in search_terms:
+    for term, ticker in zip(search_terms, norgate_tickers):
         print("Söker efter", term)
         df = ig_service.search_markets(term)
         if len(df) > 0:
             print(df)
             filtered_df = df[(df['instrumentType'] == 'CURRENCIES') | (df['instrumentType'] == 'INDICES') | (df['instrumentType'] == 'COMMODITIES') | (df['instrumentType'] == 'RATES')]
-            for epic in filtered_df['epic']:
-                epics.append(epic)
+            for _, row in filtered_df.iterrows():
+                epics.append({
+                    "name": row['instrumentName'],
+                    "epic": row['epic'],
+                    "norgate ticker": ticker
+                    })
             print(filtered_df)
             print(epics)
             print("Antal epics i lista:", len(epics))
             print("Sparar epics till Excel")
             df_epics = pd.DataFrame(epics)
             df_epics.to_excel("epics.xlsx")  
+            input("Continue?")
         else:
             markets_that_got_no_result.append(term)
         time.sleep(random.randint(5, 15))
@@ -113,14 +117,14 @@ def main():
         config.acc_type,
         acc_number=config.acc_number,
     )
-
+    
     # if you want to globally cache queries
     # ig_service = IGService(config.username, config.password, config.api_key,
     #   config.acc_type, session)
 
     ig_service.create_session()
     # ig_stream_service.create_session(version='3')
-
+    fetch_epics(ig_service)
     accounts = ig_service.fetch_accounts()
     print("accounts:\n%s" % accounts)
 
